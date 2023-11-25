38b621ddb6c9b96e9513d2077d45bf4bee5f06fa
kaidisokayna <soukainakaidi20@gmail.Com>
2023-11-25 18:01:28 +0100

    premier commit

---
+++ js/guide.js
@@ -0,0 +1,33 @@
+
+// pour afficher le tooltip avec le nom de la ville au survol des régions
+var regions = document.querySelectorAll('.map-overlay path');
+var tooltip = document.getElementById('tooltip');
+
+
+// Informations sur les villes associées aux régions
+
+
+regions.forEach(function(region) {
+  region.addEventListener('mouseenter', function(event) {
+    var cityName = region.id; // ObtenIR le nom de la ville associé à la région
+    var regionBox = region.getBoundingClientRect(); // Obtenir les dimensions de la région
+    var tooltipWidth = tooltip.offsetWidth;
+    var tooltipHeight = tooltip.offsetHeight;
+    
+    var regionCenterX = regionBox.left + regionBox.width / 2;
+    var regionCenterY = regionBox.top + regionBox.height / 2;
+    
+    tooltip.style.display = 'block';
+    tooltip.innerHTML = cityName; // Afficher le nom de la ville
+    
+    // Positionner le tooltip au centre de la région
+    tooltip.style.left = regionCenterX - tooltipWidth / 2 + 'px';
+    tooltip.style.top = regionCenterY - tooltipHeight / 2 + 'px';
+  });
+
+  region.addEventListener('mouseleave', function() {
+    tooltip.style.display = 'none';
+  });
+});
+
+
